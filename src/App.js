b3cef275d1bb7c3169c1e7ac557fd058919c19e5
swathi2020416 <poreddy.swathi@capgemini.com>
2020-09-30 21:04:08 +0530

    Fourth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
-import CalculatorApp from "./Assignment/Calculator/CalculatorApp"
+//import CalculatorApp from "./Assignment/Calculator/CalculatorApp"
 
 //import Helloworld from './Assignment/Helloworld';
 //import New from "./Assignment/Addopreration/New"
+import Clock from "./Assignment/DigitalClock/Clock"
 
 function App() {
   return (
     <div className="App">
      {/* <Helloworld/> */}
      {/* <New/> */}
-    <CalculatorApp/>
+    {/* <CalculatorApp/> */}
+    <Clock/>
     </div>
   );
 }
---
+++ src/Assignment/DigitalClock/Clock.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import Switch from "react-switch";
+
+class Clock extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { checked: false};
+        this.showHide = this.showHide.bind(this)
+    
+        this.state = {
+            time: new Date(),
+           
+        }
+    }
+    
+    changeName(){
+        let date = new Date().toLoaleString
+        date += this.state.show === true ? "hide" : "show";
+        return date;
+    }
+    showHide(checked){
+        const { show } = this.state;
+        
+        this.setState( { show : !show})
+        this.setState({checked})
+    }
+
+    
+    
+    // state = {
+    //     time: new Date()
+    //     //curTime : new Date().toLocaleString(),
+    //   };
+     
+
+    render() {
+       
+        return (
+            <div>
+               <h1 style={{backgroundColor: "lightblue"}}>Digital Clock</h1> 
+                 <h2> {this.state.time.toLocaleTimeString()}</h2> 
+                 <div className="container">
+                    <div>
+                        <div>
+                            {/* <button onClick={this.showHide} className="button-primary btn">{this.changeName()}</button> */}
+                            <Switch  id="normal-switch" onChange={this.showHide} checked={this.state.checked}/>
+                            { this.state.show && 
+                                <div>
+                                     <h2> {this.state.time.toLocaleString()}</h2> 
+                                
+                                </div>
+                            }
+                          
+                        </div> 
+                       
+               
+                
+               
+            </div>
+            </div>
+            </div>
+
+
+
+        )
+    }
+}
+
+export default Clock
